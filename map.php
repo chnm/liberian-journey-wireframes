e2838540b08fe7fc0ce393e16b9b24d0b23ecd42
kalbers <ken.albers@gmail.com>
2014-07-31 18:56:05 -0400

    Added share page

--- map.php
+++ map.php
@@ -2,14 +2,18 @@
 
 <div class="row">
 
-  <div class="large-9 column">
-    <img src="http://placehold.it/1000x600&text=MAP" alt=""/>
-  </div>
-
-  <div class="large-3 column">
+  <div class="large-3 column right">
   <h3>Using the Map</h3>
     <p>Some explanatory text about how to use the map. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis maiores voluptatum, atque consequuntur eveniet et aut similique? Dolore sint distinctio recusandae, molestiae magni nesciunt repellat modi nam consequuntur odio beatae?</p>
   </div>
+
+    <div class="large-9 column left">
+    <img src="http://placehold.it/1000x600&text=MAP" alt=""/>
+  </div>
+
 </div>
+
+
+
 
 <?php require('footer.html'); ?>
---
+++ share.php
@@ -0,0 +1,58 @@
+<?php require('header.html'); ?>
+
+<div class="row">
+    <div class="large-12 columns">
+        <h2>Place to Share Stories</h2>
+        <form> 
+            <div class="row">
+            <div class="large-10 column">
+                <p>Some text about adding your own story to the site. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt alias excepturi possimus, quibusdam adipisci nulla consequuntur ipsa. Non, voluptas saepe a omnis nesciunt, minima inventore debitis amet sint, modi, molestiae!</p>
+            </div>
+            
+                <div class="large-8"> 
+                    <div class="row"> 
+                        <div class="large-2 columns"> 
+                        <label for="right-label" class="right inline">Label</label> 
+                        </div>
+
+                        <div class="large-10 columns"> 
+                        <input type="text" id="right-label" placeholder="Inline Text Input"> 
+                        </div>
+                    </div> 
+
+                    <div class="row"> 
+                        <div class="large-2 columns"> 
+                        <label for="right-label" class="right inline">Label</label> 
+                        </div>
+
+                        <div class="large-10 columns"> 
+                        <input type="text" id="right-label" placeholder="Inline Text Input"> 
+                        </div>
+                    </div> 
+
+                    <div class="row"> 
+                        <div class="large-2 columns"> 
+                        <label for="right-label" class="right inline">Label</label> 
+                        </div>
+
+                        <div class="large-10 columns"> 
+                        <input type="text" id="right-label" placeholder="Inline Text Input"> 
+                        </div>
+                    </div> 
+
+                    <div class="row"> 
+                        <div class="large-2 columns"> 
+                        <label for="right-label" class="right inline">Label</label> 
+                        </div>
+
+                        <div class="large-10 columns"> 
+                        <textarea id="right-label" rows="10" placeholder="Inline Text Input"></textarea>
+                        </div>
+                    </div> 
+                </div>
+            </div> 
+        </form>
+    </div>
+</div>
+
+<?php require('footer.html'); ?>
